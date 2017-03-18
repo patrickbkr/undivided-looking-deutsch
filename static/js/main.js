b9281811142828b47f306e29452b88f0ee265b80
Patrick Sebastian Zimmermann <patrick@zakweb.de>
2017-03-18 10:11:45 +0100

    Change address.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var x = ['e', 'd.', 'oet', 'so', 'p', 'm', 'izp'];
-    x.splice(5, 0, '@');
+    var x = ['e', 'd.', 'be', 'w', 'ye', 'lsa', 'eb'];
+    x.splice(4, 0, '@');
     var k = x.join("").split("").reverse().join("");
     var element = document.getElementById('my-e-mail')
     if ( element !== null ) {
